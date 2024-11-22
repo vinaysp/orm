8ceba7e7df2d32d5b13d6c4d01ad3305d4122c9f
Vinay Patel <vinayspatel0@gmail.com>
2024-11-22 12:30:49 +0000

    feat: run getsessionFactory

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,18 @@
 package org.example;
 
+import Entities.Category;
+import Entities.Recipe;
+
 public class Main {
 
     public static void main(String[] args) {
 
         var sessionFactory = Database.getSessionFactory();
         sessionFactory.getSchemaManager().exportMappedObjects(true);
+
+        Category category = new Category(1,"Cars");
+        System.out.println(category.name);
+
 
 
 
